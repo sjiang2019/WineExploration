04e003625798eff1d7624edc350e963ac3b0c527
tdipadova3rd <tony.19@dartmouth.edu>
2018-05-04 17:21:27 -0400

    Style changes and removed models

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -6,12 +6,8 @@ admin.autodiscover()
 
 import hello.views
 
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]
