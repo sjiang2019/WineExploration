0c0ad58c2cb0bfd3c21ffbd7a7a1efc023b2120f
Steven Jiang <Steven@Stevens-MacBook-Pro-2.local>
2018-05-26 18:26:35 -0400

    updated settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-import sklearn
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -40,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sklearn',
     'hello'
 
 ]
